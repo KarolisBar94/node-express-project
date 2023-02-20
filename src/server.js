84c8bdb49e5b792dddbb7da3123663f96308bbd3
“KarolisBar94“ <karolis.baronas@gmail.com>
2023-02-20 10:30:22 +0200

    create env

--- src/server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const server = express();
-server.use(express.static('public'));
-
-
-server.listen(5008, () => {
-    console.log('server is running on: http://localhost:5008')
-})
